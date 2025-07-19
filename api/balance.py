10f067ae21add3571655dccd6c35ab2e9e0e2538
SuniL5250 <sunilfriends10@gmail.com>
2025-07-19 12:44:50 +0530

    final: inline network name

--- api/balance.py
+++ api/balance.py
@@ -3,7 +3,7 @@ from http.server import BaseHTTPRequestHandler
 
 BITQUERY_KEY = os.getenv("BITQUERY_KEY")
 
-CHAIN_MAP = {  # exact camel-case names Bitquery expects
+CHAIN_MAP = {
     "eth": "Ethereum",
     "polygon": "Polygon",
     "bsc": "BSC",
@@ -20,36 +20,28 @@ class handler(BaseHTTPRequestHandler):
             body = json.loads(self.rfile.read(int(self.headers['Content-Length'])))
             addr  = body["address"]
             date  = body["date"] + "T00:00:00Z"
-            network = CHAIN_MAP[body["chain"]]
+            net   = CHAIN_MAP[body["chain"]]
 
-            payload = {
-                "query": """
-query($a:String!,$d:ISO8601DateTime,$net:EthereumNetwork!){
-  ethereum(network:$net){
-    address(address:{is:$a}){
-      balances(time:{before:$d},currency:{}){
-        currency{symbol}
+            query = f"""
+{{
+  ethereum(network: {net}) {{
+    address(address: "{addr}") {{
+      balances(time: {{before: "{date}"}}, currency: {{}}) {{
+        currency {{symbol}}
         value
-      }
-    }
-  }
-}""",
-                "variables": {"a": addr, "d": date, "net": network}
-            }
+      }}
+    }}
+  }}
+}}"""
 
             req = urllib.request.Request(
                 "https://streaming.bitquery.io/graphql",
-                data=json.dumps(payload).encode(),
+                data=query.encode(),
                 headers={"Content-Type": "application/json", "X-API-KEY": BITQUERY_KEY}
             )
             res = json.loads(urllib.request.urlopen(req).read())
 
-            # safe traversal
-            data_root = res.get("data", {})
-            ethereum = data_root.get("ethereum", {})
-            address  = ethereum.get("address")
-            balances = address[0]["balances"] if address else []
-
+            balances = res.get("data", {}).get("ethereum", {}).get("address", [{}])[0].get("balances", [])
             if not balances:
                 self.send_response(200)
                 self.send_header("Content-Type", "text/plain")
