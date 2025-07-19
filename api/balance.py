fc535ede3e18d551aab51306e97bd2292f8afebe
SuniL5250 <sunilfriends10@gmail.com>
2025-07-19 12:07:49 +0530

    clean handler

--- api/balance.py
+++ api/balance.py
@@ -2,32 +2,41 @@ import json, os, urllib.request, urllib.error
 from http.server import BaseHTTPRequestHandler
 
 BITQUERY_KEY = os.getenv("BITQUERY_KEY", "free-demo-key")
+
 CHAIN_MAP = {
-    "eth":"ethereum","polygon":"polygon","bsc":"bsc",
-    "arbitrum":"arbitrum","optimism":"optimism","base":"base",
-    "avalanche":"avalanche","fantom":"fantom"
+    "eth": "ethereum",
+    "polygon": "polygon",
+    "bsc": "bsc",
+    "arbitrum": "arbitrum",
+    "optimism": "optimism",
+    "base": "base",
+    "avalanche": "avalanche",
+    "fantom": "fantom",
 }
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
         body = json.loads(self.rfile.read(int(self.headers['Content-Length'])))
-        addr, date, chain = body["address"], body["date"] + "T00:00:00Z", CHAIN_MAP[body["chain"]]
+        addr  = body["address"]
+        date  = body["date"] + "T00:00:00Z"
+        chain = CHAIN_MAP[body["chain"]]
 
-        query = {"query":"""
-        query($a:String!,$d:ISO8601DateTime){
-          ethereum(network:%s){
-            address(address:{is:$a}){
-              balances(time:{before:$d},currency:{}){
-                currency{symbol} value
-              }
-            }
-          }
-        }""" % chain, "variables":{"a":addr,"d":date}}
+        query = {"query": f"""
+        query($a:String!,$d:ISO8601DateTime){{
+          ethereum(network:{chain}){{
+            address(address:{{is:$a}}){{
+              balances(time:{{before:$d}},currency:{{}}){{
+                currency{{symbol}}
+                value
+              }}
+            }}
+          }}
+        }}""", "variables": {"a": addr, "d": date}}
 
         req = urllib.request.Request(
             "https://streaming.bitquery.io/graphql",
             data=json.dumps(query).encode(),
-            headers={"Content-Type":"application/json","X-API-KEY":BITQUERY_KEY}
+            headers={"Content-Type": "application/json", "X-API-KEY": BITQUERY_KEY}
         )
         res = json.loads(urllib.request.urlopen(req).read())
         data = res["data"]["ethereum"]["address"][0]["balances"]
